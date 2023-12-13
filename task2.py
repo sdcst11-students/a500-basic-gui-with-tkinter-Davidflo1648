8f8101603519438ce4618f3f17b2f15efb553b33
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-12-13 13:00:40 -0800

    task2 done

--- task2.py
+++ task2.py
@@ -10,10 +10,51 @@ dogPic=tk.PhotoImage(file="dog.png")
 flabel1=tk.Label(window,image=dogPic)
 flabel2=tk.Label(window,text="Client Database")
 fbutton1=tk.Button(window,text="Search By Name")
-fentry1=tk.Entry(window,text="Entry widgets can be typed in",width=10)
+fentry1=tk.Entry(window,width=18)
+
+nF2=tk.Frame()
+
+flabel3=tk.Label(window,text="Name")
+flabel4=tk.Label(window,text="Type")
+flabel5=tk.Label(window,text="Breed")
+flabel6=tk.Label(window,text="Owner")
+flabel7=tk.Label(window,text="Birthdate")
+
+nF3=tk.Frame()
+
+fentry2=tk.Entry(window,width=10)
+fentry3=tk.Entry(window,width=10)
+fentry4=tk.Entry(window,width=10)
+fentry5=tk.Entry(window,width=10)
+fentry6=tk.Entry(window,width=10)
+
+nF4=tk.Frame()
+
+fbutton2=tk.Button(window,text="< Previous")
+fbutton3=tk.Button(window,text="Next >")
 
 nF1.grid()
-flabel1.grid(row=1,column=1,columnspan=3)
-flabel2.grid(row=2,column=3)
+flabel1.grid(row=0,column=0,columnspan=3)
+flabel2.grid(row=1,column=3)
+fbutton1.grid(row=0,column=4)
+fentry1.grid(row=0,column=5)
+
+nF2.grid()
+flabel3.grid(row=5,column=1)
+flabel4.grid(row=5,column=2)
+flabel5.grid(row=5,column=3)
+flabel6.grid(row=5,column=4)
+flabel7.grid(row=5,column=5)
+
+nF3.grid()
+fentry2.grid(row=6,column=1)
+fentry3.grid(row=6,column=2)
+fentry4.grid(row=6,column=3)
+fentry5.grid(row=6,column=4)
+fentry6.grid(row=6,column=5)
+
+nF4.grid()
+fbutton2.grid(row=7,column=1)
+fbutton3.grid(row=7,column=5)
 
 window.mainloop()
