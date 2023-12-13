e658598d431b57a0bcefb5a2045cc137796f56fc
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-12-13 13:35:12 -0800

    task4 started

---
+++ task4.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+window=tk.Tk()
+window.title('Example')
+window.geometry('260x134')
+
+dogPic=tk.PhotoImage(file="dog.png")
+flabel1=tk.Label(window,image=dogPic)
+flabel2=tk.Label(window,text='Pochacco!')
+flabel3=tk.Label(window,text='A cuddly little puppy! This is from the same\n creators who brought you Keropi and Kero Kero',background='#aaffff')
+
+flabel1.place(x=0,y=0)
+flabel2.place()
+flabel3.place()
+
+window.mainloop()
