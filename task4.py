e1c1942755b7b67d7a35b8ae808e3b290b92a715
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-12-18 13:22:43 -0800

    update

--- task4.py
+++ task4.py
@@ -9,8 +9,8 @@ flabel1=tk.Label(window,image=dogPic)
 flabel2=tk.Label(window,text='Pochacco!')
 flabel3=tk.Label(window,text='A cuddly little puppy! This is from the same\n creators who brought you Keropi and Kero Kero',background='#aaffff')
 
-flabel1.place(x=0,y=0)
-flabel2.place()
+flabel1.place(x=75,y=50)
+flabel2.place(x=100,y=50)
 flabel3.place()
 
 window.mainloop()
